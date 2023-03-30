3569b0c2e951023f0c5319d5574e5ebe9bb6688b
Vic <ji3g4vic03@gmail.com>
2023-03-30 15:17:29 +0800

    Initial Commit

---
+++ teachingAids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teachingAids
+//
+//  Created by 王衍智 on 2023/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
